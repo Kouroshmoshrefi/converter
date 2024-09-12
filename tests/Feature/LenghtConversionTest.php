b92d70d3e361cc8b25908b1c5f17b11f8a4a572d
kouroshmoshrefi <kouroshmoshrefi@hotmail.com>
2024-09-12 19:23:04 +0330

    Fix method typo

--- tests/Feature/LenghtConversionTest.php
+++ tests/Feature/LenghtConversionTest.php
@@ -3,11 +3,11 @@
 use Sunstrike\Converter\Convert;
 
 test('Test converting lengths: Kilometers to Miles', function () {
-    $miles = Convert::lenght(120)->from('km')->to('mi');
+    $miles = Convert::length(120)->from('km')->to('mi');
     expect($miles)->toBeNumeric();
 });
 
 test('Test converting lengths: Miles to Kilometers', function () {
-    $kilometers = Convert::lenght(60)->from('mi')->to('km');
+    $kilometers = Convert::length(60)->from('mi')->to('km');
     expect($kilometers)->toBeNumeric();
 });
